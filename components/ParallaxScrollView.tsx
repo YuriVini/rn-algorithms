0c664a4a820b4c78c3a394c54a6378a35c9970f9
YuriVini <yuri.ybgt.yfo@gmail.com>
2024-08-05 16:46:20 -0300

    feat: remove overflow to zIndex works

--- components/ParallaxScrollView.tsx
+++ components/ParallaxScrollView.tsx
@@ -64,12 +64,10 @@ const styles = StyleSheet.create({
   },
   header: {
     height: 250,
-    overflow: 'hidden',
   },
   content: {
     flex: 1,
     padding: 32,
     gap: 16,
-    overflow: 'hidden',
   },
 });
